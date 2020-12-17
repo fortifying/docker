7bce25490f37c06c2c1849851bf2271501abe99d
Heinz Der Flugel <heinzdf@outlook.com>
2020-12-17 04:52:39 +0000

    mmf

--- fortizers/modules/memes.py
+++ fortizers/modules/memes.py
@@ -286,6 +286,114 @@ def spongemocktext(update, context):
 
 @spamcheck
 @run_async
+def mmf(update, context):
+    if not event.reply_to_msg_id:
+        await event.edit(
+            "`Syntax: reply to an image with .mmf` 'text on top' ; 'text on bottom' "
+        )
+        return
+ 
+    reply_message = await event.get_reply_message()
+    if not reply_message.media:
+        await event.edit("```reply to a image/sticker/gif```")
+        return
+    await bot.download_file(reply_message.media)
+    if event.is_reply:
+        data = await check_media(reply_message)
+        if isinstance(data, bool):
+            await event.edit("`Unsupported Files...`")
+            return
+ 
+        await event.edit(
+            "```Transfiguration Time! Mwahaha Memifying this image! (」ﾟﾛﾟ)｣ ```"
+        )
+        await asyncio.sleep(5)
+        text = event.pattern_match.group(1)
+        if event.reply_to_msg_id:
+            file_name = "meme.jpg"
+            to_download_directory = ./downloads/
+            downloaded_file_name = os.path.join(
+                to_download_directory, file_name)
+            downloaded_file_name = await bot.download_media(
+                reply_message, downloaded_file_name,
+            )
+            dls_loc = downloaded_file_name
+        webp_file = await draw_meme_text(dls_loc, text)
+        await event.client.send_file(event.chat_id, webp_file, reply_to=event.reply_to_msg_id)
+        await event.delete()
+        os.remove(webp_file)
+        os.remove(dls_loc)
+ 
+ 
+async def draw_meme_text(image_path, text):
+    img = Image.open(image_path)
+    os.remove(image_path)
+    i_width, i_height = img.size
+    m_font = ImageFont.truetype(
+        "resources/MutantAcademyStyle.ttf", int((70 / 640) * i_width))
+    if ";" in text:
+        upper_text, lower_text = text.split(";")
+    else:
+        upper_text = text
+        lower_text = ''
+    draw = ImageDraw.Draw(img)
+    current_h, pad = 10, 5
+    if upper_text:
+        for u_text in textwrap.wrap(upper_text, width=15):
+            u_width, u_height = draw.textsize(u_text, font=m_font)
+ 
+            draw.text(xy=(((i_width - u_width) / 2) - 1, int((current_h / 640)
+                                                             * i_width)), text=u_text, font=m_font, fill=(0, 0, 0))
+            draw.text(xy=(((i_width - u_width) / 2) + 1, int((current_h / 640)
+                                                             * i_width)), text=u_text, font=m_font, fill=(0, 0, 0))
+            draw.text(xy=((i_width - u_width) / 2,
+                          int(((current_h / 640) * i_width)) - 1),
+                      text=u_text,
+                      font=m_font,
+                      fill=(0,
+                            0,
+                            0))
+            draw.text(xy=(((i_width - u_width) / 2),
+                          int(((current_h / 640) * i_width)) + 1),
+                      text=u_text,
+                      font=m_font,
+                      fill=(0,
+                            0,
+                            0))
+ 
+            draw.text(xy=((i_width - u_width) / 2, int((current_h / 640)
+                                                       * i_width)), text=u_text, font=m_font, fill=(255, 255, 255))
+            current_h += u_height + pad
+    if lower_text:
+        for l_text in textwrap.wrap(lower_text, width=15):
+            u_width, u_height = draw.textsize(l_text, font=m_font)
+ 
+            draw.text(
+                xy=(((i_width - u_width) / 2) - 1, i_height - u_height - int((20 / 640) * i_width)),
+                text=l_text, font=m_font, fill=(0, 0, 0))
+            draw.text(
+                xy=(((i_width - u_width) / 2) + 1, i_height - u_height - int((20 / 640) * i_width)),
+                text=l_text, font=m_font, fill=(0, 0, 0))
+            draw.text(
+                xy=((i_width - u_width) / 2, (i_height - u_height - int((20 / 640) * i_width)) - 1),
+                text=l_text, font=m_font, fill=(0, 0, 0))
+            draw.text(
+                xy=((i_width - u_width) / 2, (i_height - u_height - int((20 / 640) * i_width)) + 1),
+                text=l_text, font=m_font, fill=(0, 0, 0))
+ 
+            draw.text(
+                xy=((i_width - u_width) / 2, i_height - u_height - int((20 / 640) * i_width)),
+                text=l_text, font=m_font, fill=(255, 255, 255))
+            current_h += u_height + pad
+ 
+    image_name = "memify.webp"
+    webp_file = os.path.join(./downloads/, image_name)
+    img.save(webp_file, "WebP")
+    return webp_file
+
+
+@spamcheck
+@run_async
 def clapmoji(update, context):
     message = update.effective_message
     if not message.reply_to_message:
@@ -392,6 +500,7 @@ MOCK_HANDLER = DisableAbleCommandHandler("mock", spongemocktext, pass_args=True)
 OWO_HANDLER = DisableAbleCommandHandler("owo", owo, pass_args=True)
 FORBES_HANDLER = DisableAbleCommandHandler("forbes", forbesify, pass_args=True)
 STRETCH_HANDLER = DisableAbleCommandHandler("stretch", stretch, pass_args=True)
+MEMEIFY_HANDLER = DisableAbleCommandHandler("mmf", mmf, pass_args=True)
 VAPOR_HANDLER = DisableAbleCommandHandler("vapor", vapor, pass_args=True)
 ZALGO_HANDLER = DisableAbleCommandHandler("zalgofy", zalgotext, pass_args=True)
 SHOUT_HANDLER = DisableAbleCommandHandler("shout", shout, pass_args=True)
@@ -410,4 +519,4 @@ dispatcher.add_handler(FORBES_HANDLER)
 dispatcher.add_handler(CHINESEMEMES_HANDLER)
 dispatcher.add_handler(MOCK_HANDLER)
 dispatcher.add_handler(DEEPFRY_HANDLER)
-
+dispatcher.add_handler(MEMEIFY_HANDLER)
